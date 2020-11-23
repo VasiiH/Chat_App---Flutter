fa0d9dd19fc6bac36562cacb52365ae48a17d84c
vasi <vasihh404@gmail.com>
2020-11-23 22:58:03 +0530

    debug

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,13 @@ import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-<<<<<<< HEAD
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(FlashChat());
 }
-=======
 
-void main() => runApp(FlashChat());
->>>>>>> origin/master
 
 class FlashChat extends StatelessWidget {
 
